230af8527b63bc05d027ff36f4b55746fb1035ec
maegatro <shuntaromaekawa@gmail.com>
2020-04-26 16:05:06 +0900

    saving progress

--- migrations/20200426103114_create_insidefridge_table.js
+++ migrations/20200426103114_create_insidefridge_table.js
@@ -4,7 +4,7 @@ exports.up = function(knex, Promise) {
     table.increments();
     table.text('name');
     table.integer('number');
-    table.date('expire_date');
+    table.string('expire_date');
   });
 };
 
